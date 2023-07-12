b1df425e88bc0ae27751eb6860a938d164459c3a
silviacamenzind <silvia.camenzind@epfl.ch>
2023-07-12 08:56:01 +0200

    working dashboard (chatgpt is great : )

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
-# to show Dashboard input, plot, dropdown menue
+# to show Dashboard input, plot, dropdown menue 
+# thanks chatgpt
 
 import dash
 from dash import html
@@ -17,8 +18,11 @@ fig = px.scatter(df, x="sepal_width", y="sepal_length")
 
 app.layout = html.Div([
     html.H1("My Dashboard"),
-    #dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown', placeholder="Select a city",),
-    dcc.Dropdown(['NYC', 'MTL', 'SF'], id='demo-dropdown', placeholder="Select a city",),
+    dcc.Dropdown(options=[{'label': 'NYC', 'value': 'NYC'},
+                          {'label': 'MTL', 'value': 'MTL'},
+                          {'label': 'SF', 'value': 'SF'}],
+                 id='demo-dropdown',
+                 placeholder="Select a city"),
     html.Div(id='dd-output-container'),
     dcc.DatePickerRange(
         start_date_placeholder_text="Start Period",
@@ -36,19 +40,17 @@ app.layout = html.Div([
 
     dcc.Graph(figure=fig)
 ])
- 
-if __name__ == "__main__":
-    app.run_server(host='localhost',port=8005, debug=True)
 
-@callback(
+
+@app.callback(
     Output('dd-output-container', 'children'),
     Input('demo-dropdown', 'value')
 )
 def update_output(value):
-     return   'You have selected {value}'
+    return f'You have selected {value}'
 
 
-@callback(
+@app.callback(
     Output(component_id='my-output', component_property='children'),
     Input(component_id='my-input', component_property='value')
 )
@@ -56,6 +58,5 @@ def update_output_div(input_value):
     return f'Output: {input_value}'
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(host='localhost', port=8050, debug=True)
